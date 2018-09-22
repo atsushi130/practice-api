b7669682771854a69cd0bf6ec29211c4e150d0ae
atsushi130 <atsushi_main@outlook.jp>
2018-09-23 00:29:08 +0900

    temp: ItemController

---
+++ src/main/kotlin/com/github/atsushi130/practice/ItemController.kt
@@ -0,0 +1,21 @@
+package com.github.atsushi130.practice
+
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.annotation.RequestScope
+import org.springframework.web.bind.annotation.RequestMethod.GET
+
+@RestController
+@RequestScope
+@Component
+class ItemController {
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/items", method = [GET])
+    fun getItems(): String {
+        return "Items"
+    }
+}
