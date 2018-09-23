c270f19e58432aa85b78d4483f97813ed8a7873f
atsushi130 <atsushi_main@outlook.jp>
2018-09-23 20:31:42 +0900

    fix: findBy method of user model

--- src/main/kotlin/com/github/atsushi130/practice/domain/models/User.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/models/User.kt
@@ -2,7 +2,7 @@ package com.github.atsushi130.practice.domain.models
 
 data class User(val id: String) {
     companion object {
-        fun findBy(sessionId: String): User {
+        fun findBy(id: String): User {
             return User("1")
         }
     }
