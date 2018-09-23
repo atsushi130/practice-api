976adcdca120d6954aec27cfe8e3832cd12c7a35
atsushi130 <atsushi_main@outlook.jp>
2018-09-23 17:20:32 +0900

    implement: ItemResource

---
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/resources/ItemResource.kt
@@ -0,0 +1,22 @@
+package com.github.atsushi130.practice.presentation.resources
+
+import com.github.atsushi130.practice.domain.models.Item
+import java.io.Serializable
+
+data class ItemResource(
+        val id: String,
+        val name: String,
+        val subName: String
+): Serializable {
+
+    companion object {
+
+        fun from(model: Item): ItemResource = ItemResource(model.id, model.name, model.subName)
+
+        fun from(models: List<Item>): List<ItemResource> {
+            return models.map {
+                ItemResource.from(it)
+            }
+        }
+    }
+}
