522aaeb62ad2a77eed401f4f7c98c8e840ce8648
atsushi130 <atsushi_main@outlook.jp>
2018-09-23 21:49:16 +0900

    remove: ItemUseCase

--- src/main/kotlin/com/github/atsushi130/practice/domain/usecases/items/ItemUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.github.atsushi130.practice.domain.usecases.items
-
-import com.github.atsushi130.practice.domain.models.Item
-
-interface ItemUseCase {
-    fun getItems(): List<Item>
-}
--- src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/ItemPresenterImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/ItemPresenterImpl.kt
@@ -1,7 +1,6 @@
 package com.github.atsushi130.practice.presentation.presenters
 
 import com.github.atsushi130.practice.domain.shared.UserContainer
-import com.github.atsushi130.practice.domain.usecases.items.ItemUseCase
 import com.github.atsushi130.practice.presentation.resources.ItemResource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
