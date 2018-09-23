a6d4e808c7b80f513f3533a70d305a753162eeb9
atsushi130 <atsushi_main@outlook.jp>
2018-09-23 17:20:19 +0900

    implement: ItemUseCase

---
+++ src/main/kotlin/com/github/atsushi130/practice/domain/usecases/items/ItemUseCaseImpl.kt
@@ -0,0 +1,14 @@
+package com.github.atsushi130.practice.domain.usecases.items
+
+import com.github.atsushi130.practice.domain.models.Item
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class ItemUseCaseImpl: ItemUseCase {
+
+    override fun getItems(): List<Item> {
+        val item = Item("1", "name1", "subName1")
+        return Arrays.asList(item)
+    }
+}
