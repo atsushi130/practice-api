18a19f6d33e56572f2a12f7e8af57b3055a5fb26
atsushi130 <atsushi_main@outlook.jp>
2018-09-23 20:43:55 +0900

    implement: items property for user and add document

--- src/main/kotlin/com/github/atsushi130/practice/domain/models/User.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/models/User.kt
@@ -1,6 +1,14 @@
 package com.github.atsushi130.practice.domain.models
 
+/**
+ * primary key: id
+ * user has many items
+ */
 data class User(val id: String) {
+
+    val items: List<Item>
+        get() = Item.findBy(this)
+
     companion object {
         fun findBy(id: String): User? {
             return User("1")
