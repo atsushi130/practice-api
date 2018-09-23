656cbe5e24666cd745d56d516019d811343be0ec
atsushi130 <atsushi_main@outlook.jp>
2018-09-23 21:37:24 +0900

    implement: SessionRepository

---
+++ src/main/kotlin/com/github/atsushi130/practice/data/repository/SessionRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package com.github.atsushi130.practice.data.repository
+
+import com.github.atsushi130.practice.domain.models.Session
+import com.github.atsushi130.practice.domain.repository.SessionRepository
+
+class SessionRepositoryImpl {
+    companion object: SessionRepository {
+        override fun findBy(id: String): Session? {
+            if (id == "5755BECA-1A85-4714-AF0C-4ECE06E5BE16") {
+                return Session(id, "F2940B42-20AC-4F74-918F-479FCD547FF5")
+            }
+            return null
+        }
+    }
+}
