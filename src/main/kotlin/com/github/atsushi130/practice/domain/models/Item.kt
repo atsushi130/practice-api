8bae5abec9b04297acf20b47034c0e56dce93516
atsushi130 <atsushi_main@outlook.jp>
2018-09-23 20:43:22 +0900

    implement: findBy method for item model

--- src/main/kotlin/com/github/atsushi130/practice/domain/models/Item.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/models/Item.kt
@@ -1,5 +1,7 @@
 package com.github.atsushi130.practice.domain.models
 
+import java.util.*
+
 data class Item(
     val id: String,
     val userId: String,
@@ -7,8 +9,14 @@ data class Item(
     val subName: String
 ) {
     companion object {
+
         fun findBy(id: String): Item? {
             return Item(id, "1", "name1", "subName1")
+        }
+
+        fun findBy(user: User): List<Item> {
+            val item = Item(user.id, "1", "name1", "subName1")
+            return Arrays.asList(item)
         }
     }
 }
