14fa91a92a4b3a07d61e3343be8ae118fc85eaec
atsushi130 <atsushi_main@outlook.jp>
2018-09-24 17:45:43 +0900

    implement: account presenter

---
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/AccountPresenter.kt
@@ -0,0 +1,20 @@
+package com.github.atsushi130.practice.presentation.presenters
+
+import com.github.atsushi130.practice.domain.usecases.accounts.CreateAccountUseCase
+import com.github.atsushi130.practice.exception.AccountException
+import com.github.atsushi130.practice.presentation.resources.AccountResource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class AccountPresenter {
+
+    @Autowired
+    private lateinit var createAccountUseCase: CreateAccountUseCase
+
+    @Throws(AccountException::class)
+    fun create(account: AccountResource) {
+        val model = account.toModel()
+        this.createAccountUseCase.execute(model)
+    }
+}
