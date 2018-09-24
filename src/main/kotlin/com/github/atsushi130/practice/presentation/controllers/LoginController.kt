8f2fef5db32fbd2846413cf6c677d8515abdff9b
atsushi130 <atsushi_main@outlook.jp>
2018-09-24 18:51:01 +0900

    implement: signup and signin method

--- src/main/kotlin/com/github/atsushi130/practice/presentation/controllers/LoginController.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/controllers/LoginController.kt
@@ -1,9 +1,10 @@
 package com.github.atsushi130.practice.presentation.controllers
 
 import com.github.atsushi130.practice.exception.AccountException
-import com.github.atsushi130.practice.presentation.presenters.AccountPresenter
+import com.github.atsushi130.practice.presentation.presenters.LoginPresenter
 import com.github.atsushi130.practice.presentation.resources.AccountResource
 import com.github.atsushi130.practice.presentation.resources.ErrorResource
+import com.github.atsushi130.practice.presentation.resources.LoginResource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
@@ -18,13 +19,15 @@ import java.lang.Exception
 class LoginController {
 
     @Autowired
-    private lateinit var presenter: AccountPresenter
+    private lateinit var presenter: LoginPresenter
 
     @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = "/account/create", method = [POST])
-    fun createAccount(@RequestBody account: AccountResource) = this.presenter.create(account)
+    @RequestMapping(value = "/sign_up", method = [POST])
+    fun signUp(@RequestBody account: AccountResource): LoginResource = this.presenter.signUp(account)
 
-    fun
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/sign_in", method = [POST])
+    fun signIn(@RequestBody account: AccountResource): LoginResource = this.presenter.signIn(account)
 
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
