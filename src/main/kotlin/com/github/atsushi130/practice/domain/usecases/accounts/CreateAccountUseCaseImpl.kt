cecf168a3b621b5c25ce9514f4ea851d2929bd5e
atsushi130 <atsushi_main@outlook.jp>
2018-09-24 17:45:21 +0900

    implement: create account use case

---
+++ src/main/kotlin/com/github/atsushi130/practice/domain/usecases/accounts/CreateAccountUseCaseImpl.kt
@@ -0,0 +1,11 @@
+package com.github.atsushi130.practice.domain.usecases.accounts
+
+import com.github.atsushi130.practice.domain.models.Account
+import org.springframework.stereotype.Service
+
+@Service
+class CreateAccountUseCaseImpl: CreateAccountUseCase {
+
+    override fun create(account: Account) {
+    }
+}
