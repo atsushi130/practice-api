7c120e141d30261a0d32b9452804e27195a493ec
atsushi130 <atsushi_main@outlook.jp>
2018-09-24 18:47:05 +0900

    remove: validation method of account resource

--- src/main/kotlin/com/github/atsushi130/practice/domain/models/Account.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/models/Account.kt
@@ -1,3 +1,27 @@
 package com.github.atsushi130.practice.domain.models
 
-data class Account(val account: String, val password: String)
+import com.github.atsushi130.practice.exception.AccountException
+
+data class Account(val userId: String, val password: String) {
+
+    @Throws(AccountException::class)
+    fun create() {
+        this.validate()
+    }
+
+    @Throws(AccountException::class)
+    fun validate() {
+        this.validateAccount()
+        this.validatePassword()
+    }
+
+    @Throws(AccountException.AccountRuleNotEnough::class)
+    private fun validateAccount() {}
+
+    @Throws(AccountException.PasswordRuleNotEnough::class)
+    private fun validatePassword() {
+        if (password.length !in 6..18) {
+            throw AccountException.PasswordRuleNotEnough()
+        }
+    }
+}
--- src/main/kotlin/com/github/atsushi130/practice/presentation/resources/AccountResource.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/resources/AccountResource.kt
@@ -1,30 +1,10 @@
 package com.github.atsushi130.practice.presentation.resources
 
 import com.github.atsushi130.practice.domain.models.Account
-import com.github.atsushi130.practice.exception.AccountException
 import java.io.Serializable
 
-data class AccountResource(val account: String, val password: String): Serializable {
-
-    @Throws(AccountException::class)
+data class AccountResource(val userId: String, val password: String): Serializable {
     fun toModel(): Account {
-        this.validate()
-        return Account(this.account, this.password)
-    }
-
-    @Throws(AccountException::class)
-    fun validate() {
-        this.validateAccount()
-        this.validatePassword()
-    }
-
-    @Throws(AccountException.AccountRuleNotEnough::class)
-    private fun validateAccount() {}
-
-    @Throws(AccountException.PasswordRuleNotEnough::class)
-    private fun validatePassword() {
-        if (password.length !in 6..18) {
-            throw AccountException.PasswordRuleNotEnough()
-        }
+        return Account(this.userId, this.password)
     }
 }
