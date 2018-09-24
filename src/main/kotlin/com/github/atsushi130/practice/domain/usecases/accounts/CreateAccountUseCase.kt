9eb9a706d49d70175c397057f9a705aa101d6a9c
atsushi130 <atsushi_main@outlook.jp>
2018-09-24 18:49:14 +0900

    modify: execute method of create account use case

--- src/main/kotlin/com/github/atsushi130/practice/domain/usecases/accounts/CreateAccountUseCase.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/usecases/accounts/CreateAccountUseCase.kt
@@ -1,7 +1,9 @@
 package com.github.atsushi130.practice.domain.usecases.accounts
 
 import com.github.atsushi130.practice.domain.models.Account
+import javax.security.auth.login.AccountException
 
 interface CreateAccountUseCase {
+    @Throws(AccountException::class)
     fun execute(account: Account)
 }
