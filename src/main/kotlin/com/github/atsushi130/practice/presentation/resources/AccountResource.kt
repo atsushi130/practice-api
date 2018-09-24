0c160b56fd861fa246e9c4e96e43919f17d5bf32
atsushi130 <atsushi_main@outlook.jp>
2018-09-24 17:41:07 +0900

    implement: validation

--- src/main/kotlin/com/github/atsushi130/practice/presentation/resources/AccountResource.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/resources/AccountResource.kt
@@ -1,5 +1,30 @@
 package com.github.atsushi130.practice.presentation.resources
 
+import com.github.atsushi130.practice.domain.models.Account
+import com.github.atsushi130.practice.exception.AccountException
 import java.io.Serializable
 
-data class LoginResource(val account: String?, val password: String?): Serializable
+data class AccountResource(val account: String, val password: String): Serializable {
+
+    @Throws(AccountException::class)
+    fun toModel(): Account {
+        this.validate()
+        return Account(this.account, this.password)
+    }
+
+    @Throws(AccountException::class)
+    fun validate() {
+        this.validateAccount()
+        this.validatePassword()
+    }
+
+    @Throws(AccountException.AccountRuleNotEnough::class)
+    private fun validateAccount() {}
+
+    @Throws(AccountException.PasswordRuleNotEnough::class)
+    private fun validatePassword() {
+        if (password.length !in 6..18) {
+            throw AccountException.PasswordRuleNotEnough()
+        }
+    }
+}
