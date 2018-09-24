5488f00ba83dc502debee664fdc93f1d340bdca7
atsushi130 <atsushi_main@outlook.jp>
2018-09-24 17:41:24 +0900

    implement: Exception handler

--- src/main/kotlin/com/github/atsushi130/practice/presentation/controllers/AccountController.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/controllers/AccountController.kt
@@ -1,20 +1,34 @@
 package com.github.atsushi130.practice.presentation.controllers
 
-import com.github.atsushi130.practice.presentation.resources.LoginResource
+import com.github.atsushi130.practice.exception.AccountException
+import com.github.atsushi130.practice.presentation.presenters.AccountPresenter
+import com.github.atsushi130.practice.presentation.resources.AccountResource
+import com.github.atsushi130.practice.presentation.resources.ErrorResource
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.context.annotation.RequestScope
 import org.springframework.web.bind.annotation.RequestMethod.POST
+import java.lang.Exception
 
 @RestController
 @RequestScope
 @Component
-class LoginController {
+class AccountController {
+
+    @Autowired
+    private lateinit var presenter: AccountPresenter
 
     @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = "/login", method = [POST])
-    fun login(@RequestBody loginResource: LoginResource) {
-        println(loginResource)
-    }
+    @RequestMapping(value = "/account/create", method = [POST])
+    fun create(@RequestBody account: AccountResource) = this.presenter.create(account)
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun badRequest(exception: Exception): ErrorResource = ErrorResource.from(exception)
+
+    @ExceptionHandler(AccountException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun badRequest(exception: AccountException): ErrorResource = ErrorResource.from(exception)
 }
