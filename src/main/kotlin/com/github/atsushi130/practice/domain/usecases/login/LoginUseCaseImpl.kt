1cabb2c91b240b35f33b672f3e59f6fd458ef9f1
atsushi130 <atsushi_main@outlook.jp>
2018-09-24 18:50:27 +0900

    implement: login use case

---
+++ src/main/kotlin/com/github/atsushi130/practice/domain/usecases/login/LoginUseCaseImpl.kt
@@ -0,0 +1,10 @@
+package com.github.atsushi130.practice.domain.usecases.login
+
+import com.github.atsushi130.practice.domain.models.Account
+import org.springframework.stereotype.Service
+
+@Service
+class LoginUseCaseImpl: LoginUseCase {
+    override fun execute(account: Account) {
+    }
+}
