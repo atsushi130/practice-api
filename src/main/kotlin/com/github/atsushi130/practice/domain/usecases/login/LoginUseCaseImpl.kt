52dac7fb390b7d55a73e3f25154ff1a64267f3a9
atsushi130 <atsushi_main@outlook.jp>
2018-09-25 22:46:32 +0900

    implement: execute method

--- src/main/kotlin/com/github/atsushi130/practice/domain/usecases/login/LoginUseCaseImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/usecases/login/LoginUseCaseImpl.kt
@@ -1,10 +1,15 @@
 package com.github.atsushi130.practice.domain.usecases.login
 
 import com.github.atsushi130.practice.domain.models.Account
+import com.github.atsushi130.practice.domain.models.Session
+import com.github.atsushi130.practice.exception.LoginException
 import org.springframework.stereotype.Service
 
 @Service
 class LoginUseCaseImpl: LoginUseCase {
-    override fun execute(account: Account) {
+
+    @Throws(LoginException::class)
+    override fun execute(account: Account): Session {
+        return Session.findByUserId(account.userId) ?: throw LoginException.LoginFailure()
     }
 }
