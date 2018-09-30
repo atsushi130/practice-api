40381d7bd3bc3b9b9a3336d4c8c00ee007d886e9
atsushi130 <atsushi_main@outlook.jp>
2018-09-30 18:57:53 +0900

    define: Authentication exception

--- src/main/kotlin/com/github/atsushi130/practice/exception/AuthenticationException.kt
+++ src/main/kotlin/com/github/atsushi130/practice/exception/AuthenticationException.kt
@@ -1,7 +1,9 @@
 package com.github.atsushi130.practice.exception
 
 sealed class AuthenticationException(override val code: Int, override val message: String): PracticeException(code, message) {
-    class CookieNotExists: AuthenticationException(1000, "cookie does not exists.")
-    class SessionIdNotExists: AuthenticationException(1001, "sessionId does not exists.")
-    class InvalidSessionId: AuthenticationException(1002, "invalid sessionId.")
+    class AuthenticationHeaderNotExists: AuthenticationException(1000, "authentication header does not exists.")
+    class BearTokenNotExists: AuthenticationException(1001, "bearer token does not exists.")
+    class CookieNotExists: AuthenticationException(1002, "cookie does not exists.")
+    class SessionIdNotExists: AuthenticationException(1003, "sessionId does not exists.")
+    class InvalidSessionId: AuthenticationException(1004, "invalid sessionId.")
 }
