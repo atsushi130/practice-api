cafd8c0f31b6c32fcb0956bafb4c841b4bdecb09
atsushi130 <atsushi_main@outlook.jp>
2018-11-18 23:33:21 +0900

    implement: find funs by method

--- src/main/kotlin/com/github/atsushi130/practice/data/repositories/UserRepositoryImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/data/repositories/UserRepositoryImpl.kt
@@ -2,14 +2,18 @@ package com.github.atsushi130.practice.data.repositories
 
 import com.github.atsushi130.practice.domain.models.User
 import com.github.atsushi130.practice.domain.repositories.UserRepository
+import java.util.*
 
 class UserRepositoryImpl {
     companion object: UserRepository {
+
         override fun findBy(id: String): User? {
             if (id == "atsushi130") {
                 return User(id)
             }
             return null
         }
+
+        override fun findFunsBy(itemId: String): List<User> = Arrays.asList()
     }
 }
