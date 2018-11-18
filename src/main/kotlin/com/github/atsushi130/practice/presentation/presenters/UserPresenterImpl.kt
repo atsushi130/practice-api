efa9e05bc8333e3664a273a7e39fc74d7db191d4
atsushi130 <atsushi_main@outlook.jp>
2018-11-18 23:34:19 +0900

    implement: use user use case

--- src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/UserPresenterImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/UserPresenterImpl.kt
@@ -1,11 +1,16 @@
 package com.github.atsushi130.practice.presentation.presenters
 
+import com.github.atsushi130.practice.domain.usecases.users.UserUseCase
 import com.github.atsushi130.practice.presentation.resources.UserResource
+import com.github.atsushi130.practice.presentation.resources.UserResource.Companion.from
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import java.util.*
 
 @Component
 class UserPresenterImpl: UserPresenter {
 
-    override fun getFunsBy(itemId: String): List<UserResource> = Arrays.asList()
+    @Autowired
+    private lateinit var useCase: UserUseCase
+
+    override fun findFunsBy(itemId: String): List<UserResource> = this.useCase.findFunsBy(itemId).map(::from)
 }
