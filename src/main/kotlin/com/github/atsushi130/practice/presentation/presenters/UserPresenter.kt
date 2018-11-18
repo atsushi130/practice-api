93944ee3636e2bed5ee0fb362f574c9931fa842a
atsushi130 <atsushi_main@outlook.jp>
2018-11-18 23:33:57 +0900

    modify: method name

--- src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/UserPresenter.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/UserPresenter.kt
@@ -3,5 +3,5 @@ package com.github.atsushi130.practice.presentation.presenters
 import com.github.atsushi130.practice.presentation.resources.UserResource
 
 interface UserPresenter {
-    fun getFunsBy(itemId: String): List<UserResource>
+    fun findFunsBy(itemId: String): List<UserResource>
 }
