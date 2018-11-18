f8d710015d12e8532d5205f4dd172761a4752429
atsushi130 <atsushi_main@outlook.jp>
2018-11-18 22:36:51 +0900

    fix: indent

--- src/main/kotlin/com/github/atsushi130/practice/data/repositories/ItemRepositoryImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/data/repositories/ItemRepositoryImpl.kt
@@ -13,14 +13,16 @@ class ItemRepositoryImpl {
         }
 
         override fun findBy(user: User): List<Item> {
-            return Arrays.asList(
-                Item("1", "F2940B42-20AC-4F74-918F-479FCD547FF5", "name1", "subName1"),
-                Item("2", "DC024697-443B-4182-8CBD-8837ACBF7E23", "name2", "subName2"),
-                Item("3", "7DDB6B1C-57F7-489A-95FF-2FC7AF12C046", "name3", "subName3"),
-                Item("4", "F2940B42-20AC-4F74-918F-479FCD547FF5", "name4", "subName4")
-            ).filter {
-                it.userId == user.id
-            }
+            return Arrays
+                .asList(
+                    Item("1", "F2940B42-20AC-4F74-918F-479FCD547FF5", "name1", "subName1"),
+                    Item("2", "DC024697-443B-4182-8CBD-8837ACBF7E23", "name2", "subName2"),
+                    Item("3", "7DDB6B1C-57F7-489A-95FF-2FC7AF12C046", "name3", "subName3"),
+                    Item("4", "F2940B42-20AC-4F74-918F-479FCD547FF5", "name4", "subName4")
+                )
+                .filter {
+                    it.userId == user.id
+                }
         }
     }
 }
