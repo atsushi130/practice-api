c9e2ed2cd2bc0b75a222c947bf123800e83a2d68
atsushi130 <atsushi_main@outlook.jp>
2018-11-23 19:00:00 +0900

    define: getUser method

--- src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/UserPresenter.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/UserPresenter.kt
@@ -3,5 +3,6 @@ package com.github.atsushi130.practice.presentation.presenters
 import com.github.atsushi130.practice.presentation.resources.UserResource
 
 interface UserPresenter {
+    fun getUser(): UserResource
     fun findFunsBy(itemId: String): List<UserResource>
 }
