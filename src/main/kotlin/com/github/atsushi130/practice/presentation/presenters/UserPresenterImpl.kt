dbdee9c822b1dc1db1d3b2fba979999cc034503f
atsushi130 <atsushi_main@outlook.jp>
2018-11-23 19:00:15 +0900

    implement: getUser method

--- src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/UserPresenterImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/UserPresenterImpl.kt
@@ -1,5 +1,6 @@
 package com.github.atsushi130.practice.presentation.presenters
 
+import com.github.atsushi130.practice.domain.shared.UserContainer
 import com.github.atsushi130.practice.domain.usecases.users.UserUseCase
 import com.github.atsushi130.practice.presentation.resources.UserResource
 import com.github.atsushi130.practice.presentation.resources.UserResource.Companion.from
@@ -9,8 +10,15 @@ import org.springframework.stereotype.Component
 @Component
 class UserPresenterImpl: UserPresenter {
 
+
+    @Autowired
+    private lateinit var userContainer: UserContainer
+
     @Autowired
     private lateinit var useCase: UserUseCase
+
+    override fun getUser(): UserResource = UserResource.from(this.userContainer.user)
+
 
     override fun findFunsBy(itemId: String): List<UserResource> = this.useCase.findFunsBy(itemId).map(::from)
 }
