b756442cda2def44845f9e399176c5b03a312fc6
atsushi130 <atsushi_main@outlook.jp>
2018-11-25 22:10:46 +0900

    modify: Item resource properties and implement from method

--- src/main/kotlin/com/github/atsushi130/practice/presentation/resources/ItemResource.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/resources/ItemResource.kt
@@ -6,9 +6,19 @@ import java.io.Serializable
 data class ItemResource(
     val id: String,
     val name: String,
-    val subName: String
+    val subName: String,
+    val registeredUser: UserResource,
+    val latestReactionUser: UserResource?,
+    val wants: ReactionResource,
+    val haves: ReactionResource
 ): Serializable {
     companion object {
-        fun from(model: Item): ItemResource = ItemResource(model.id, model.name, model.subName)
+        fun from(model: Item): ItemResource {
+            val registeredUser = UserResource.from(model.registeredUser)
+            val latestReactionUser = model.latestReactionUser?.let { UserResource.from(it) }
+            val wants = ReactionResource.from(model.wants)
+            val haves = ReactionResource.from(model.haves)
+            return ItemResource(model.id, model.name, model.subName, registeredUser, latestReactionUser, wants, haves)
+        }
     }
 }
