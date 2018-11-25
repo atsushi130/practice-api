113fe748e02cea5921826d6052805dfbbad9e5e4
atsushi130 <atsushi_main@outlook.jp>
2018-11-25 22:26:09 +0900

    implement: getLatestItems to ItemPresenter

--- src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/ItemPresenterImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/presenters/ItemPresenterImpl.kt
@@ -1,6 +1,7 @@
 package com.github.atsushi130.practice.presentation.presenters
 
 import com.github.atsushi130.practice.domain.shared.UserContainer
+import com.github.atsushi130.practice.domain.usecases.items.ItemUseCase
 import com.github.atsushi130.practice.presentation.resources.ItemResource
 import com.github.atsushi130.practice.presentation.resources.ItemResource.Companion.from
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,5 +14,10 @@ class ItemPresenterImpl: ItemPresenter {
     @Autowired
     private lateinit var userContainer: UserContainer
 
+    @Autowired
+    private lateinit var useCase: ItemUseCase
+
     override fun getItems(): List<ItemResource> = this.userContainer.user.items.map(::from)
+
+    override fun getLatestItems(): List<ItemResource> = this.useCase.getLatestItems().map(::from)
 }
