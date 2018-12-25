e202df4c4d678d22bf20a721e50e6ab114489732
atsushi130 <atsushi_main@outlook.jp>
2018-12-25 22:11:26 +0900

    modify: code of AuthenticationException

--- src/main/kotlin/com/github/atsushi130/practice/exception/AuthenticationException.kt
+++ src/main/kotlin/com/github/atsushi130/practice/exception/AuthenticationException.kt
@@ -1,7 +1,7 @@
 package com.github.atsushi130.practice.exception
 
-sealed class AuthenticationException(override val code: Int, override val message: String): PracticeException(code, message) {
-    class CookieNotExists: AuthenticationException(1000, "cookie does not exists.")
-    class SessionIdNotExists: AuthenticationException(1001, "sessionId does not exists.")
-    class InvalidSessionId: AuthenticationException(1002, "invalid sessionId.")
+sealed class AuthenticationException(override val code: ExceptionCode, override val message: String): PracticeException(code, message) {
+    class CookieNotExists: AuthenticationException(ExceptionCode.Authentication(0), "cookie does not exists.")
+    class SessionIdNotExists: AuthenticationException(ExceptionCode.Authentication(1), "sessionId does not exists.")
+    class InvalidSessionId: AuthenticationException(ExceptionCode.Authentication(2), "invalid sessionId.")
 }
