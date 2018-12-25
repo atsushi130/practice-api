9f257571ee0c85cafb135c6061508db0602ce759
atsushi130 <atsushi_main@outlook.jp>
2018-12-25 22:12:01 +0900

    modify: code of PracticeException

--- src/main/kotlin/com/github/atsushi130/practice/exception/PracticeException.kt
+++ src/main/kotlin/com/github/atsushi130/practice/exception/PracticeException.kt
@@ -1,3 +1,3 @@
 package com.github.atsushi130.practice.exception
 
-open class PracticeException(open val code: Int, override val message: String): Exception(message)
+open class PracticeException(open val code: ExceptionCode, override val message: String): Exception(message)
