72d12ef26284dd4a0bbff8484940f9a86418d187
atsushi130 <atsushi_main@outlook.jp>
2018-12-25 22:18:16 +0900

    implement: UserDeviceRepository

---
+++ src/main/kotlin/com/github/atsushi130/practice/data/repositories/UserDeviceRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.github.atsushi130.practice.data.repositories
+
+import com.github.atsushi130.practice.domain.models.*
+import com.github.atsushi130.practice.domain.repositories.UserDeviceRepository
+
+/**
+ * Schema
+ * userId: Int
+ * osType: String
+ * osVersion: String
+ * appVersion: String
+ */
+class UserDeviceRepositoryImpl {
+    companion object: UserDeviceRepository {
+
+        override fun findBy(userId: String): UserDevice? {
+            val osVersion = OSVersion("12.0.0")
+            val appVersion = iOSAppVersion("1.0.0")
+            return UserDevice(userId, osVersion, appVersion)
+        }
+
+        override fun update(userDevice: UserDevice) {
+        }
+    }
+}
