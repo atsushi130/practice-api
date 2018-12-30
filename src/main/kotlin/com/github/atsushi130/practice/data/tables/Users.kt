8af1fd74129311239abde3c868f46b0de0d8e42c
atsushi130 <atsushi_main@outlook.jp>
2018-12-31 03:05:22 +0900

    implement: Users table

---
+++ src/main/kotlin/com/github/atsushi130/practice/data/tables/Users.kt
@@ -0,0 +1,16 @@
+package com.github.atsushi130.practice.data.tables
+
+import org.jetbrains.exposed.sql.Table
+
+/**
+ * Migration
+ *
+ * create table users (
+ *   id char(36) not null primary key,
+ *   name not null unique
+ * );
+ */
+object Users: Table() {
+    val id   = varchar("id", 36).primaryKey()
+    val name = text("name").uniqueIndex()
+}
