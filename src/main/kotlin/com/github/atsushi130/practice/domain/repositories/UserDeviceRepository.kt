6ca2d0c30073ed85da8a3b25d2b45548d3f1873b
atsushi130 <atsushi_main@outlook.jp>
2019-01-01 23:41:05 +0900

    modify: UserDeviceRepository interface

--- src/main/kotlin/com/github/atsushi130/practice/domain/repositories/UserDeviceRepository.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/repositories/UserDeviceRepository.kt
@@ -3,6 +3,6 @@ package com.github.atsushi130.practice.domain.repositories
 import com.github.atsushi130.practice.domain.models.UserDevice
 
 interface UserDeviceRepository {
-    fun findBy(userId: String): UserDevice?
+    fun findBy(userId: String): List<UserDevice>
     fun update(userDevice: UserDevice)
 }
