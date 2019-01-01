36241c9d0ff7d518739825600809532115d823f5
atsushi130 <atsushi_main@outlook.jp>
2019-01-01 20:30:30 +0900

    implement: use user entity

--- src/main/kotlin/com/github/atsushi130/practice/data/repositories/UserRepositoryImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/data/repositories/UserRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.github.atsushi130.practice.data.repositories
 
 import com.github.atsushi130.practice.data.dataAccessObjects.UserDao
+import com.github.atsushi130.practice.data.dataAccessObjects.Users
+import com.github.atsushi130.practice.data.entities.UserEntity
 import com.github.atsushi130.practice.domain.models.User
 import com.github.atsushi130.practice.domain.repositories.UserRepository
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 
@@ -13,14 +16,16 @@ class UserRepositoryImpl: UserRepository {
     private lateinit var dao: UserDao
 
     override fun findBy(id: String): User? {
-        return this.dao.findBy(id)?.let {
-            it.toModel()
+        return transaction {
+            UserEntity
+                .find { Users.id eq id }
+                .singleOrNull()
+                ?.toModel()
         }
     }
 
     override fun findFunsBy(itemId: Int): List<User> {
-        return this.dao.findFunsBy(itemId)
-            .map { it.toModel() }
+        return emptyList()
     }
 
 }
