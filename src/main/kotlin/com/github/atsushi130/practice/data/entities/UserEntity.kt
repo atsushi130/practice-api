50a2cd4f0597b424a6734f4da6006406031362a4
atsushi130 <atsushi_main@outlook.jp>
2019-01-01 22:57:12 +0900

    implement: userDevices property to UserEntity

--- src/main/kotlin/com/github/atsushi130/practice/data/entities/UserEntity.kt
+++ src/main/kotlin/com/github/atsushi130/practice/data/entities/UserEntity.kt
@@ -1,5 +1,6 @@
 package com.github.atsushi130.practice.data.entities
 
+import com.github.atsushi130.practice.data.tables.UserDevices
 import com.github.atsushi130.practice.data.tables.Users
 import com.github.atsushi130.practice.domain.models.User
 import org.jetbrains.exposed.dao.Entity
@@ -9,6 +10,13 @@ import org.jetbrains.exposed.dao.EntityID
 class UserEntity(id: EntityID<String>): Entity<String>(id) {
 
     companion object : EntityClass<String, UserEntity>(Users)
+
+    val userDevices: List<UserDeviceEntity>
+        get() {
+            return UserDeviceEntity
+                .find { UserDevices.userId eq this@UserEntity.id.value }
+                .toList()
+        }
 
     fun toModel(): User {
         return User(this.id.value)
