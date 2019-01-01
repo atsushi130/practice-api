da37ba866be8c6b50b73982469871c368f79a461
atsushi130 <atsushi_main@outlook.jp>
2019-01-01 20:08:19 +0900

    implement: use session dao

--- src/main/kotlin/com/github/atsushi130/practice/data/repositories/SessionRepositoryImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/data/repositories/SessionRepositoryImpl.kt
@@ -1,25 +1,22 @@
 package com.github.atsushi130.practice.data.repositories
 
-import com.github.atsushi130.practice.data.tables.SessionEntity
-import com.github.atsushi130.practice.data.tables.Sessions
+import com.github.atsushi130.practice.data.dataAccessObjects.SessionDao
 import com.github.atsushi130.practice.domain.models.Session
 import com.github.atsushi130.practice.domain.repositories.SessionRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 
 @Repository
 class SessionRepositoryImpl: SessionRepository {
 
+    @Autowired
+    private lateinit var dao: SessionDao
+
     override fun findBySessionId(id: String): Session? {
-        return SessionEntity
-            .find { Sessions.id eq id }
-            .firstOrNull()
-            ?.toModel()
+        return this.dao.findBySessionId(id)?.toModel()
     }
 
     override fun findByUserId(userId: String): Session? {
-        return SessionEntity
-            .find { Sessions.userId eq userId }
-            .firstOrNull()
-            ?.toModel()
+        return this.dao.findByUserId(userId)?.toModel()
     }
 }
