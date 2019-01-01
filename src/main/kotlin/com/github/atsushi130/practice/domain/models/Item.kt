3fc4db294b093bd32b350544c3521c3bb7e73375
atsushi130 <atsushi_main@outlook.jp>
2019-01-02 00:55:56 +0900

    remove: companion object of Item

--- src/main/kotlin/com/github/atsushi130/practice/domain/models/Item.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/models/Item.kt
@@ -1,8 +1,5 @@
 package com.github.atsushi130.practice.domain.models
 
-import com.github.atsushi130.practice.data.repositories.ItemRepositoryImpl
-import com.github.atsushi130.practice.domain.repositories.ItemRepository
-
 data class Item(
     val id: Int,
     val name: String,
@@ -10,15 +7,4 @@ data class Item(
     val registeredUserId: String,
     val wants: Reaction,
     val haves: Reaction
-) {
-    companion object {
-
-        private val repository: ItemRepository = ItemRepositoryImpl
-
-        fun findBy(id: Int): Item? = this.repository.findBy(id)
-
-        fun findBy(userId: String): List<Item> = this.repository.findBy(userId)
-
-        fun getLatest(): List<Item> = this.repository.getLatestItems()
-    }
-}
+)
