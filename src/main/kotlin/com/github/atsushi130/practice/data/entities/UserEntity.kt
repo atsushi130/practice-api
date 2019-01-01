e59da48b91f059052db3a48e2e793cd881b53056
atsushi130 <atsushi_main@outlook.jp>
2019-01-01 20:23:48 +0900

    define: user entity

---
+++ src/main/kotlin/com/github/atsushi130/practice/data/entities/UserEntity.kt
@@ -0,0 +1,16 @@
+package com.github.atsushi130.practice.data.entities
+
+import com.github.atsushi130.practice.data.dataAccessObjects.Users
+import com.github.atsushi130.practice.domain.models.User
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.dao.EntityClass
+import org.jetbrains.exposed.dao.EntityID
+
+class UserEntity(id: EntityID<String>): Entity<String>(id) {
+
+    companion object : EntityClass<String, UserEntity>(Users)
+
+    fun toModel(): User {
+        return User(this.id.value)
+    }
+}
