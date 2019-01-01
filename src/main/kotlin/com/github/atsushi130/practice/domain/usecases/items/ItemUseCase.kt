e1c4e6f2a3d3a02e1d11127422c22908a7dd195a
atsushi130 <atsushi_main@outlook.jp>
2019-01-02 00:57:13 +0900

    add: getItems method to ItemUseCase interface

--- src/main/kotlin/com/github/atsushi130/practice/domain/usecases/items/ItemUseCase.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/usecases/items/ItemUseCase.kt
@@ -3,5 +3,6 @@ package com.github.atsushi130.practice.domain.usecases.items
 import com.github.atsushi130.practice.domain.models.Item
 
 interface ItemUseCase {
+    fun getItems(): List<Item>
     fun getLatestItems(): List<Item>
 }
