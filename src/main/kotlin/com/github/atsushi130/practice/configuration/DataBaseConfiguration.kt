dfe1fb5272470068af4e81f6cb9320543a1a3571
atsushi130 <atsushi_main@outlook.jp>
2019-01-01 17:10:31 +0900

    refactoring: DatabaseConfiguration init

--- src/main/kotlin/com/github/atsushi130/practice/configuration/DataBaseConfiguration.kt
+++ src/main/kotlin/com/github/atsushi130/practice/configuration/DataBaseConfiguration.kt
@@ -1,10 +1,13 @@
 package com.github.atsushi130.practice.configuration
 
+import com.github.atsushi130.practice.data.tables.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.transaction.annotation.TransactionManagementConfigurer
 import org.jetbrains.exposed.spring.SpringTransactionManager
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils.create
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.sql.DataSource
@@ -14,7 +17,18 @@ import javax.sql.DataSource
 class DataBaseConfiguration(@Autowired val dataSource: DataSource): TransactionManagementConfigurer {
 
     init {
+        this.connectDatabase()
+        this.createTablesIfNeeded()
+    }
+
+    private fun connectDatabase() {
         Database.connect(this.dataSource)
+    }
+
+    private fun createTablesIfNeeded() {
+        transaction {
+            create(Users, Sessions)
+        }
     }
 
     @Bean
