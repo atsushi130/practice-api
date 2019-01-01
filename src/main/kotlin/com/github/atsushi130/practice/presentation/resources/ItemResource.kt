7607c9b754e7db9c2bff1a5253c37191ff6b4a17
atsushi130 <atsushi_main@outlook.jp>
2019-01-02 00:51:20 +0900

    modify: ItemResource implementation

--- src/main/kotlin/com/github/atsushi130/practice/presentation/resources/ItemResource.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/resources/ItemResource.kt
@@ -4,21 +4,18 @@ import com.github.atsushi130.practice.domain.models.Item
 import java.io.Serializable
 
 data class ItemResource(
-    val id: String,
+    val id: Int,
     val name: String,
     val subName: String,
-    val registeredUser: UserResource,
-    val latestReactionUser: UserResource?,
+    val registeredUserId: String,
     val wants: ReactionResource,
     val haves: ReactionResource
 ): Serializable {
     companion object {
         fun from(model: Item): ItemResource {
-            val registeredUser = UserResource.from(model.registeredUser)
-            val latestReactionUser = model.latestReactionUser?.let { UserResource.from(it) }
             val wants = ReactionResource.from(model.wants)
             val haves = ReactionResource.from(model.haves)
-            return ItemResource(model.id, model.name, model.subName, registeredUser, latestReactionUser, wants, haves)
+            return ItemResource(model.id, model.name, model.subName, model.registeredUserId, wants, haves)
         }
     }
 }
