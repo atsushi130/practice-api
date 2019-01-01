f0d6c850411b9769ea3ca2e97c4cf996a7759dbd
atsushi130 <atsushi_main@outlook.jp>
2019-01-01 20:24:15 +0900

    remove: user entity from user table

--- src/main/kotlin/com/github/atsushi130/practice/data/dataAccessObjects/Users.kt
+++ src/main/kotlin/com/github/atsushi130/practice/data/dataAccessObjects/Users.kt
@@ -1,16 +1,7 @@
 package com.github.atsushi130.practice.data.dataAccessObjects
 
-import com.github.atsushi130.practice.domain.models.User
-import org.jetbrains.exposed.dao.*
+import org.jetbrains.exposed.dao.IdTable
 
 object Users: IdTable<String>("users") {
     override val id = varchar("id", 36).primaryKey().entityId()
-}
-
-class UserEntity(id: EntityID<String>): Entity<String>(id) {
-    companion object : EntityClass<String, UserEntity>(Users)
-
-    fun toModel(): User {
-        return User(this.id.value)
-    }
 }
