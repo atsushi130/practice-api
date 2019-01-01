0e37914892f0d3fd1665fd5e85d1d916ced10606
atsushi130 <atsushi_main@outlook.jp>
2019-01-01 19:59:51 +0900

    modify: user property name

--- src/main/kotlin/com/github/atsushi130/practice/data/tables/Sessions.kt
+++ src/main/kotlin/com/github/atsushi130/practice/data/tables/Sessions.kt
@@ -1,5 +1,6 @@
 package com.github.atsushi130.practice.data.tables
 
+import com.github.atsushi130.practice.domain.models.Session
 import org.jetbrains.exposed.dao.Entity
 import org.jetbrains.exposed.dao.EntityClass
 import org.jetbrains.exposed.dao.EntityID
@@ -7,10 +8,16 @@ import org.jetbrains.exposed.dao.IdTable
 
 object Sessions: IdTable<String>("sessions") {
     override val id = varchar("id", 36).primaryKey().entityId()
-    val user = reference("user_id", Users)
+    val userId = reference("user_id", Users)
 }
 
 class SessionEntity(id: EntityID<String>): Entity<String>(id) {
+
     companion object: EntityClass<String, SessionEntity>(Sessions)
-    var user by UserEntity referencedOn Sessions.user
+    var user by UserEntity referencedOn Sessions.userId
+
+    fun toModel(): Session {
+        val user = this.user.toModel()
+        return Session(this.id.value, user)
+    }
 }
