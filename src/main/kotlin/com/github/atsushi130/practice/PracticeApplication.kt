ef9c75e952a32caa9b88e1d834b8a8d7711ab798
atsushi130 <atsushi_main@outlook.jp>
2019-01-01 19:59:14 +0900

    remove: sample code

--- src/main/kotlin/com/github/atsushi130/practice/PracticeApplication.kt
+++ src/main/kotlin/com/github/atsushi130/practice/PracticeApplication.kt
@@ -1,11 +1,5 @@
 package com.github.atsushi130.practice
 
-import com.github.atsushi130.practice.data.tables.SessionEntity
-import com.github.atsushi130.practice.data.tables.Sessions
-import com.github.atsushi130.practice.data.tables.UserEntity
-import com.github.atsushi130.practice.data.tables.Users
-import org.jetbrains.exposed.sql.selectAll
-import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -16,15 +10,6 @@ class PracticeApplication {
     companion object {
         @JvmStatic fun main(vararg args: String) {
             SpringApplication.run(PracticeApplication::class.java, *args)
-            transaction {
-                SessionEntity
-                    .find {
-                        Sessions.id eq "5755BECA-1A85-4714-AF0C-4ECE06E5BE16"
-                    }
-                    .forEach {
-                        println(it.user.id.value)
-                    }
-            }
         }
     }
 }
