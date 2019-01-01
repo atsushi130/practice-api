2c4316de0964e2899483604accfc1109aa64f527
atsushi130 <atsushi_main@outlook.jp>
2019-01-01 20:02:24 +0900

    modify: session repository interface

--- src/main/kotlin/com/github/atsushi130/practice/domain/repositories/SessionRepository.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/repositories/SessionRepository.kt
@@ -4,5 +4,5 @@ import com.github.atsushi130.practice.domain.models.Session
 
 interface SessionRepository {
     fun findBySessionId(id: String): Session?
-    fun findByUserId(id: String): Session?
+    fun findByUserId(userId: String): Session?
 }
