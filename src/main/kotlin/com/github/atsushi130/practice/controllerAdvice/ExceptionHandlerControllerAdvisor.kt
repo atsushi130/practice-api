4142f2edb6ad7538925c2225c393923e5d3e765b
atsushi130 <atsushi_main@outlook.jp>
2019-01-02 17:27:06 +0900

    fix: advice annotation and class name

--- src/main/kotlin/com/github/atsushi130/practice/controllerAdvice/ExceptionHandlerControllerAdvisor.kt
+++ src/main/kotlin/com/github/atsushi130/practice/controllerAdvice/ExceptionHandlerControllerAdvisor.kt
@@ -4,13 +4,13 @@ import com.github.atsushi130.practice.exception.AuthenticationException
 import com.github.atsushi130.practice.exception.UserStateException
 import com.github.atsushi130.practice.presentation.resources.ErrorResource
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@ControllerAdvice
+@RestControllerAdvice
 @Suppress("unused")
-class ExceptionHandlerControllerAdvice {
+class ExceptionHandlerControllerAdvisor {
 
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST /* 400 */)
