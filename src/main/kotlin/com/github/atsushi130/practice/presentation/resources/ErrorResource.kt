9f3e842ab7bd1bfec05a4f4813efe2b8ccf8dffc
atsushi130 <atsushi_main@outlook.jp>
2019-01-02 17:32:15 +0900

    implement: Serializable to ErrorResource

--- src/main/kotlin/com/github/atsushi130/practice/presentation/resources/ErrorResource.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/resources/ErrorResource.kt
@@ -1,9 +1,10 @@
 package com.github.atsushi130.practice.presentation.resources
 
 import com.github.atsushi130.practice.exception.PracticeException
+import java.io.Serializable
 import java.lang.Exception
 
-data class ErrorResource(val code: Int, val message: String) {
+data class ErrorResource(val code: Int, val message: String): Serializable {
     companion object {
 
         fun from(exception: PracticeException): ErrorResource = ErrorResource(exception.code.value, exception.message)
