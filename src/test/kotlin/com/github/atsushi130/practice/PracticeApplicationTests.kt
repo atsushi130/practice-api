c0420190cec2d8581033bff70ea44cc70bab64ca
atsushi130 <atsushi_main@outlook.jp>
2019-01-03 04:36:59 +0900

    remove: test

--- src/test/kotlin/com/github/atsushi130/practice/PracticeApplicationTests.kt
+++ src/test/kotlin/com/github/atsushi130/practice/PracticeApplicationTests.kt
@@ -5,12 +5,12 @@ import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
-class PracticeApplicationTests {
+// @RunWith(SpringRunner::class)
+// @SpringBootTest
+// class PracticeApplicationTests {
+//
+//     @Test
+//     fun contextLoads() {
+//     }
 
-    @Test
-    fun contextLoads() {
-    }
-
-}
+// }
