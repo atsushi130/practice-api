f06868cd0a4c34a1731249a5a7db5d4cc01bd744
atsushi130 <atsushi_main@outlook.jp>
2019-01-04 18:37:13 +0900

    implement: SessionRepository.create

--- src/main/kotlin/com/github/atsushi130/practice/data/repositories/SessionRepositoryImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/data/repositories/SessionRepositoryImpl.kt
@@ -2,9 +2,17 @@ package com.github.atsushi130.practice.data.repositories
 
 import com.github.atsushi130.practice.data.tables.Sessions
 import com.github.atsushi130.practice.data.entities.SessionEntity
+import com.github.atsushi130.practice.data.entities.UserEntity
+import com.github.atsushi130.practice.data.tables.Users
 import com.github.atsushi130.practice.domain.models.Session
+import com.github.atsushi130.practice.domain.models.User
 import com.github.atsushi130.practice.domain.repositories.SessionRepository
+import org.jetbrains.exposed.dao.Entity
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -26,6 +34,20 @@ class SessionRepositoryImpl: SessionRepository {
                 .find { Sessions.userId eq userId }
                 .singleOrNull()
                 ?.toModel()
+        }
+    }
+
+    override fun create(id: String, user: User): Session {
+        return transaction {
+            Sessions
+                .insert {
+                    it[Sessions.id] = EntityID(id, Sessions)
+                    it[Sessions.userId] = EntityID(user.id, Users)
+                }
+            SessionEntity
+                .find { Sessions.userId eq user.id }
+                .single()
+                .toModel()
         }
     }
 }
