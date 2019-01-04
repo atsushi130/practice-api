c69d528054089e9db8ca676d792a3b7a0f0c689d
atsushi130 <atsushi_main@outlook.jp>
2019-01-04 18:39:14 +0900

    modify: UserRepository.create

--- src/main/kotlin/com/github/atsushi130/practice/domain/repositories/UserRepository.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/repositories/UserRepository.kt
@@ -5,4 +5,5 @@ import com.github.atsushi130.practice.domain.models.User
 interface UserRepository {
     fun findBy(id: String): User?
     fun findFunsBy(itemId: Int): List<User>
+    fun create(id: String): User
 }
