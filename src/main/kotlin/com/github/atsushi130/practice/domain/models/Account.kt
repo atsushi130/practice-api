74f9229256a09384ad17ca2767cfc9775059ca09
atsushi130 <atsushi_main@outlook.jp>
2019-01-04 18:35:01 +0900

    implement: temp validate account method

--- src/main/kotlin/com/github/atsushi130/practice/domain/models/Account.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/models/Account.kt
@@ -1,13 +1,9 @@
 package com.github.atsushi130.practice.domain.models
 
 import com.github.atsushi130.practice.exception.AccountException
+import com.github.atsushi130.practice.extension.alphabets
 
 data class Account(val userId: String, val password: String) {
-
-    @Throws(AccountException::class)
-    fun create() {
-        this.validate()
-    }
 
     @Throws(AccountException::class)
     fun validate() {
@@ -16,12 +12,30 @@ data class Account(val userId: String, val password: String) {
     }
 
     @Throws(AccountException.AccountRuleNotEnough::class)
-    private fun validateAccount() {}
+    private fun validateAccount() {
+        if (this.userId.length !in 1..36) throw AccountException.AccountRuleNotEnough.InvalidLength()
+        val validCharacters = Account.validCharacters + String.alphabets
+        val isValid = this.userId
+            .map { it.toString() }
+            .fold(true) { sum, character ->
+                sum && validCharacters.contains(character)
+            }
+        if (!isValid) throw AccountException.AccountRuleNotEnough.InvalidCharacter()
+    }
 
     @Throws(AccountException.PasswordRuleNotEnough::class)
     private fun validatePassword() {
         if (password.length !in 6..18) {
             throw AccountException.PasswordRuleNotEnough()
         }
+    }
+
+    companion object {
+        private val validCharacters: List<String>
+            get() {
+                return listOf(
+
+                )
+            }
     }
 }
