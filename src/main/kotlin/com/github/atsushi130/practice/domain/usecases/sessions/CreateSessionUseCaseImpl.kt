42ac30cc8a1dc19c803c5a8d6b16441ebebf8ece
atsushi130 <atsushi_main@outlook.jp>
2019-01-04 18:38:54 +0900

    implement: CreateSessionUseCase.execute

--- src/main/kotlin/com/github/atsushi130/practice/domain/usecases/sessions/CreateSessionUseCaseImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/usecases/sessions/CreateSessionUseCaseImpl.kt
@@ -1,22 +1,26 @@
-package com.github.atsushi130.practice.domain.usecases.login
+package com.github.atsushi130.practice.domain.usecases.sessions
 
-import com.github.atsushi130.practice.domain.models.Account
 import com.github.atsushi130.practice.domain.models.Session
 import com.github.atsushi130.practice.domain.models.User
 import com.github.atsushi130.practice.domain.repositories.SessionRepository
 import com.github.atsushi130.practice.exception.LoginException
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 @Suppress("unused")
-class LoginUseCaseImpl: CreateSessionUseCase {
+class CreateSessionUseCaseImpl: CreateSessionUseCase {
 
     @Autowired
     private lateinit var repository: SessionRepository
 
     @Throws(LoginException::class)
     override fun execute(user: User): Session {
-        return this.repository.findByUserId(user.id) ?: throw LoginException.LoginFailure()
+        val sessionId = UUID.randomUUID().toString()
+        return transaction {
+            return@transaction repository.create(sessionId, user)
+        }
     }
 }
