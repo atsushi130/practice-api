b2b1c83838acdcd443db2de108820d7a747e2376
atsushi130 <atsushi_main@outlook.jp>
2019-01-04 18:32:53 +0900

    modify: SessionResource

--- src/main/kotlin/com/github/atsushi130/practice/presentation/resources/SessionResource.kt
+++ src/main/kotlin/com/github/atsushi130/practice/presentation/resources/SessionResource.kt
@@ -3,8 +3,8 @@ package com.github.atsushi130.practice.presentation.resources
 import com.github.atsushi130.practice.domain.models.Session
 import java.io.Serializable
 
-data class SessionResource(val sessionId: String): Serializable {
+data class SessionResource(val sessionId: String, val userId: String): Serializable {
     companion object {
-        fun from(session: Session): SessionResource = SessionResource(session.id)
+        fun from(session: Session): SessionResource = SessionResource(session.id, session.user.id)
     }
 }
