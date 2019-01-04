33d01dddd55d388b800ac753b921012991cb8ef5
atsushi130 <atsushi_main@outlook.jp>
2019-01-04 18:37:42 +0900

    rename: to CreateSessionUseCase from LoginUseCase

--- src/main/kotlin/com/github/atsushi130/practice/domain/usecases/sessions/CreateSessionUseCase.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/usecases/sessions/CreateSessionUseCase.kt
@@ -1,11 +1,10 @@
-package com.github.atsushi130.practice.domain.usecases.login
+package com.github.atsushi130.practice.domain.usecases.sessions
 
-import com.github.atsushi130.practice.domain.models.Account
 import com.github.atsushi130.practice.domain.models.Session
 import com.github.atsushi130.practice.domain.models.User
 import com.github.atsushi130.practice.exception.LoginException
 
-interface LoginUseCase {
+interface CreateSessionUseCase {
     @Throws(LoginException::class)
     fun execute(user: User): Session
 }
