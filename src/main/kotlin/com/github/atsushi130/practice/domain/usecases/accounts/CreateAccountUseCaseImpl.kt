b8260da8f42ed529e8ae37624a414fa24eb0c795
atsushi130 <atsushi_main@outlook.jp>
2019-01-04 18:38:35 +0900

    implement: CreateAccountUseCase.execute

--- src/main/kotlin/com/github/atsushi130/practice/domain/usecases/accounts/CreateAccountUseCaseImpl.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/usecases/accounts/CreateAccountUseCaseImpl.kt
@@ -1,6 +1,15 @@
 package com.github.atsushi130.practice.domain.usecases.accounts
 
 import com.github.atsushi130.practice.domain.models.Account
+import com.github.atsushi130.practice.domain.models.User
+import com.github.atsushi130.practice.domain.models.UserDevice
+import com.github.atsushi130.practice.domain.repositories.UserDeviceRepository
+import com.github.atsushi130.practice.domain.repositories.UserPasswordRepository
+import com.github.atsushi130.practice.domain.repositories.UserRepository
+import com.github.atsushi130.practice.domain.shared.DeviceContainer
+import com.github.atsushi130.practice.domain.shared.UserContainer
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import javax.security.auth.login.AccountException
 
@@ -8,8 +17,29 @@ import javax.security.auth.login.AccountException
 @Suppress("unused")
 class CreateAccountUseCaseImpl: CreateAccountUseCase {
 
+    @Autowired
+    private lateinit var userPasswordRepository: UserPasswordRepository
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var userDeviceRepository: UserDeviceRepository
+
+    @Autowired
+    private lateinit var userContainer: UserContainer
+
+    @Autowired
+    private lateinit var deviceContainer: DeviceContainer
+
     @Throws(AccountException::class)
-    override fun execute(account: Account) {
-        account.create()
+    override fun execute(account: Account): User {
+        account.validate()
+        return transaction {
+            userPasswordRepository.create(account)
+            val user = userRepository.create(account.userId)
+            userDeviceRepository.create(user.id, deviceContainer.device)
+            return@transaction user
+        }
     }
 }
