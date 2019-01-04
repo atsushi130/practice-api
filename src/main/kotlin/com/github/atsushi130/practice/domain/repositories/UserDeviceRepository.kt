dc67913ff2e7b0e8da9d8c4bf49471157faf939e
atsushi130 <atsushi_main@outlook.jp>
2019-01-04 18:36:23 +0900

    modify: user device create interface

--- src/main/kotlin/com/github/atsushi130/practice/domain/repositories/UserDeviceRepository.kt
+++ src/main/kotlin/com/github/atsushi130/practice/domain/repositories/UserDeviceRepository.kt
@@ -1,8 +1,10 @@
 package com.github.atsushi130.practice.domain.repositories
 
+import com.github.atsushi130.practice.domain.models.Device
 import com.github.atsushi130.practice.domain.models.UserDevice
 
 interface UserDeviceRepository {
     fun findBy(userId: String): List<UserDevice>
     fun update(userDevice: UserDevice)
+    fun create(userId: String, device: Device): UserDevice
 }
