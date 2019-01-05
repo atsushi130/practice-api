801fe30ee3d78f874308252d6cd7f4c2785b2101
atsushi130 <atsushi_main@outlook.jp>
2019-01-06 01:42:17 +0900

    add: upper alphabets as String extension

--- src/main/kotlin/com/github/atsushi130/practice/extension/String+.kt
+++ src/main/kotlin/com/github/atsushi130/practice/extension/String+.kt
@@ -2,7 +2,17 @@ package com.github.atsushi130.practice.extension
 
 val String.Companion.alphabets: List<String>
     get() {
+        return String.lowerAlphabets + String.upperAlphabets
+    }
+
+val String.Companion.lowerAlphabets: List<String>
+    get() {
         return ('a'..'z').map { it.toString() }
+    }
+
+val String.Companion.upperAlphabets: List<String>
+    get() {
+        return ('A'..'Z').map { it.toString() }
     }
 
 val String.Companion.numbers: List<String>
